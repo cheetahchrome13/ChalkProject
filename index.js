af966d893264960f4401bda7f3f931527d9d3c3d
Justin Mangan <jm0015052@otc.edu>
2017-11-01 09:49:15 -0500

    update progress

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var chalk = require('chalk');
 var lodash = require('lodash');
+const file = 'data.json';
 
+var jsonfile = require('jsonfile')
+jsonfile.readFile(file, function(err, obj) {
+  console.dir(obj)
+})
 
