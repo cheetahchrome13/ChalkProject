e3dcca7336fa2bcc75b024398c4d84fcc77b9711
Justin Mangan <jm0015052@otc.edu>
2017-11-01 10:21:13 -0500

    update

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var chalk = require('chalk');
-var lodash = require('lodash');
+var _ = require('lodash');
 const file = 'data.json';
-
+var error;
 var jsonfile = require('jsonfile')
-jsonfile.readFile(file, function(err, obj) {
-  console.dir(obj)
+
+jsonfile.readFile(file, function(localError, obj) {
+    _.forEach(obj.items[0], function(value) {
+        console.log(value);
+    });
+    _.forEach(obj.items[1], function(value) {
+        console.log(value);
+    });
+  
+    console.log(obj);
+ 
 })
+
 
